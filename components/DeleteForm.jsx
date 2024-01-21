b5eed2649afe0933b96eda990dd07948ba251688
Manoj <manojgowdacs35@gmail.com>
2024-01-21 23:08:47 +0530

    delete form component

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,11 +1,23 @@
+'use client'
 import { deleteTask } from '@/utils/actions'
 import React from 'react'
+import toast from 'react-hot-toast'
+import {useFormStatus} from 'react-dom'
+
+const SubmitButton = ()=>{
+  const {pending} = useFormStatus();
+  return (
+    <button className='btn btn-xs btn-error'disabled={pending}>
+      {pending ? 'pending...':'delete'}
+      </button>
+    )
+}
 
 const DeleteForm = ({id}) => {
   return (
     <form action={deleteTask}>
         <input type="hidden" name='id' value={id} />
-        <button className='btn btn-xs btn-error'>delete</button>
+        <SubmitButton />
     </form>
   )
 }
