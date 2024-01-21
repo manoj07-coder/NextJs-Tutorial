4cda0caf0732c20d50e4adacc4e37372b7159cd0
Manoj <manojgowdacs35@gmail.com>
2024-01-21 23:04:26 +0530

    tasks functionality

--- utils/actions.js
+++ utils/actions.js
@@ -1,6 +1,8 @@
 'use server';
 import { revalidatePath } from "next/cache";
 import prisma from "./db"
+import { redirect } from "next/navigation";
+import {z} from 'zod'
 
 
 export const getAllTasks = async ()=>{
@@ -29,4 +31,50 @@ export const deleteTask = async (formData)=>{
         },
     });
     revalidatePath('/tasks');
+}
+
+export const getTask =  (id)=>{
+    return  prisma.task.findUnique({
+        where:{
+            id,
+        },
+    });
+}
+
+export const editTask = async (formData)=>{
+    const id = formData.get('id')
+    const content = formData.get('content')
+    const completed = formData.get('completed');
+    
+    await prisma.task.update({
+        where:{
+            id,
+        },
+        data:{
+            content,
+            completed:completed === 'on' ? true : false,
+        },
+    });
+    redirect('/tasks');
+}
+
+export const createTaskCustom = async (prevState,formData)=>{
+    // await new Promise((resolve)=>setTimeout(resolve,2000));
+    const content = formData.get('content');
+    const Task = z.object({
+        content:z.string().min(5)
+    })
+    try {
+        Task.parse({content});
+        await prisma.task.create({
+            data:{
+                content,
+            },
+        });
+        revalidatePath('/tasks');
+        return {message:'success'}
+    } catch (error) {
+        // console.log(error.errors[0].message);
+        return {message:'error'}
+    }
 }
