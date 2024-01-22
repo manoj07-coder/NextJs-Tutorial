4d340d198edf092cd8ad2f53b337889d2cdf27ba
Manoj <manojgowdacs35@gmail.com>
2024-01-22 22:28:59 +0530

    single drink page

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
 
-const SingleDrinkPage = ({params}) => {
-    console.log(params.id);
-  return (
-    <div>SingleDrinkPage</div>
-  )
+
+
+const getSingleDrink = async (id) => {
+    const res = await fetch(`${url}${id}`);
+    if (!res.ok) {
+        throw new Error('Failed to fetch a drink');
+    }
+    return res.json();
+}
+
+
+const SingleDrinkPage = async ({ params }) => {
+    const data = await getSingleDrink(params.id);
+    const title = data.drinks[0].strDrink;
+    const imgSrc = data.drinks[0].strDrinkThumb;
+    return (
+        <div>
+            <Link href='/drinks' className='btn btn-primary mt-8 mb-12'>
+                Back to drinks
+            </Link>
+            <Image src={imgSrc} width={300} height={300}
+            className='w-48 h-48 rounded shadow-lg mb-8' priority />
+            <h1 className='text-4xl mb-8'>{title}</h1>
+        </div>
+    )
 }
 
 export default SingleDrinkPage
