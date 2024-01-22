1db512e92cdbb63b0c195b32b83e3f9bb3804a3e
Manoj <manojgowdacs35@gmail.com>
2024-01-22 22:28:15 +0530

    prisma example page

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import prisma from '@/utils/db'
 
-const PrismaPage = () => {
+const prismaHandlers = async ()=>{
+    await prisma.task.create({
+        data:{
+            content:'wake up',
+        },
+    });
+    const allTasks = await prisma.task.findMany({
+        orderBy:{
+            createdAt:'desc',
+        },
+    });
+    return allTasks;
+}
+
+const PrismaPage = async() => {
+    const tasks = await prismaHandlers();
   return (
     <div>
-        <h1 className='text-7xl'>PrismaPage</h1>
+        <h1 className='text-4xl'>PrismaPage</h1>
+        {
+            tasks.map((task)=>{
+                return (
+                    <h2 key={task.id} className='text-xl py-2'>
+                        {task.content}
+                    </h2>
+                )
+            })
+        }
     </div>
   )
 }
